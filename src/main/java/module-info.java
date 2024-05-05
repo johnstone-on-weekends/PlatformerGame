bb5c08b112ee6f87ca348ad9b46f9d92922b7c03
Peter Johnstone <paj012@bucknell.edu>
2024-05-04 23:26:55 -0400

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module platformer.game.main {
+    requires java.base;
+    requires java.desktop;
+    requires java.sql;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+}
